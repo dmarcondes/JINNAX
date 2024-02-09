a496dd790e0e2f9b5d87bbc17f20d54b1f43da76
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-09 10:20:59 -0600

    Python package

--- JINNAX/__init__.py
+++ JINNAX/__init__.py
@@ -1,4 +1,4 @@
-import architectures
-import Main
-import proccess_results
-import training
+from architectures import architectures
+from main import main
+from proccess_results import proccess_results
+from training import training
