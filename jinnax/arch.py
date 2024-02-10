6f08630aed5f15580346c73f9d2884e21c496f01
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-09 19:07:04 -0600

    arch

--- jinnax/arch.py
+++ jinnax/arch.py
@@ -1,1 +1,30 @@
 #Functions to define NN architectures
+import jax
+import jax.numpy as jnp
+
+#Simple fully connected architecture. Return the function for the forward pass
+def fconNN(width,activation = jax.nn.tanh,key = 0):
+    #Initialize parameters with Glorot initialization
+    initializer = jax.nn.initializers.glorot_normal()
+    key = jax.random.split(jax.random.PRNGKey(key),len(width)-1) #Seed for initialization
+    params = list()
+    for key,lin,lout in zip(key,width[:-1],width[1:]):
+        W = initializer(key,(lin,lout),jnp.float32)
+        B = initializer(key,(1,lout),jnp.float32)
+        params.append({'W':W,'B':B})
+
+    #Define function for forward pass
+    def forward(params,x):
+      *hidden,output = params
+      for layer in hidden:
+        x = activation(x @ layer['W'] + layer['B'])
+      return x @ output['W'] + output['B']
+
+    #Return initial parameters and forward function
+    return {'params': params,'forward': forward}
+
+#Erosion: matrix f by a function k defined in a window W
+def erosion(f,k,d):
+w = jnp.array([[x1.tolist(),x2.tolist()] for x1 in jnp.linspace(-jnp.floor(d/2),jnp.floor(d/2),d) for x2 in jnp.linspace(-jnp.floor(d/2),jnp.floor(d/2),d)])
+k = k(w)
+k = jnp.transpose(k.reshape((3,3)))
--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,6 @@
 #Install from GitHub
 pip install git+https://github.com/dmarcondes/JINNAX
-from jinnax
+from jinnax import data as jd
 
 def u(x,t):
     return x[0] + x[1] + x[3] + t**2
@@ -16,7 +16,7 @@ sigmaB = 10
 sigmaI = 0
 sigmaS = 0
 
-dat = generate_dDimdataPINN(u,0,1,0,1,100,100,100,d = 2,posx = 'grid',post = 'grid',posc = 'grid',sigmaB = 0,sigmaI = 10,sigmaS = 0)
+dat = jd.generate_dDimdataPINN(u,0,1,0,1,100,100,100,d = 2,posx = 'grid',post = 'grid',posc = 'grid',sigmaB = 0,sigmaI = 10,sigmaS = 0)
 dat['collocation']
 dat['uboundary']
 for name,value in dat.items():
@@ -26,4 +26,15 @@ with jnp.printoptions(threshold=jnp.inf):
     print(x_boundary)
 
 files_path = ['/home/dmarcondes/Dropbox/Diego/Profissional/Code/Experiments/DMNN_experiments/data/material_science/Phi_comp_' + str(i+1) + '.jpg' for i in range(10)]
-f = '/home/dmarcondes/Pictures/d&l.jpeg'
+f = ['/home/dmarcondes/Pictures/d&l.jpeg']
+f = jd.png_to_jnp(f)
+f = f[0,:,:,0]
+
+x = jax.random.unifo
+
+rm(key = jax.random.PRNGKey(random.randint(0,sys.maxsize)),minval = 0,maxval = 1,shape = (10,5))
+f = fconNN([2,40,40,40,1])
+params = f['params']
+def k(x):
+    return f['forward'](params,x)
+forward(params,x)
