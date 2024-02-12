8098cf662a11f42a3ac290bd78e52d719aa580be
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-12 15:43:24 -0600

    jit

--- jinnax/arch.py
+++ jinnax/arch.py
@@ -16,6 +16,7 @@ def fconNN(width,activation = jax.nn.tanh,key = 0):
         params.append({'W':W,'B':B})
 
     #Define function for forward pass
+    @jit
     def forward(params,x):
       *hidden,output = params
       for layer in hidden:
@@ -40,6 +41,7 @@ def fconNN_str(width,activation = jax.nn.tanh,key = 0):
         params.append({'W':W,'B':B})
 
     #Define function for forward pass
+    @jit
     def forward(params,x):
       *hidden,output = params
       for layer in hidden:
@@ -50,6 +52,7 @@ def fconNN_str(width,activation = jax.nn.tanh,key = 0):
     return {'params': params,'forward': forward}
 
 #Apply a morphological layer
+@jit
 def apply_morph_layer(x,type,width,params,p,w,index_x,d):
     #Define which operator will be applied
     if type == 'erosion':
@@ -108,6 +111,7 @@ def apply_morph_layer(x,type,width,params,p,w,index_x,d):
     return {'x': fx,'p': p}
 
 #Canonical Morphological NN
+@jit
 def cmnn(type,width,width_str,size,shape_x,activation = jax.nn.tanh,key = 0):
     #Index window
     index_x = mp.index_array(shape_x)
--- jinnax/training.py
+++ jinnax/training.py
@@ -1,1 +1,10 @@
 #Functions to train NN
+import optax
+
+#MSE
+@jit
+def MSE(true,pred):
+  return jnp.mean((true - pred)**2)
+
+#Training function
+def train(x,forward,params,loss,epochs,batches = 1):
