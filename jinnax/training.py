cf0cd6c8890bbb0f130439a86c32f1223daa183b
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-12 18:02:48 -0600

    training batches

--- jinnax/training.py
+++ jinnax/training.py
@@ -3,6 +3,7 @@ import jax
 import jax.numpy as jnp
 import optax
 from alive_progress import alive_bar
+import math
 
 #MSE
 @jax.jit
@@ -10,7 +11,13 @@ def MSE(true,pred):
   return jnp.mean((true - pred)**2)
 
 #Training function
-def train_morph(x,y,forward,params,loss,epochs = 1,batches = 1,lr = 0.1,b1 = 0.9,b2 = 0.999,eps = 1e-08,eps_root = 0.0):
+def train_morph(x,y,forward,params,loss,epochs = 1,batches = 1,lr = 0.1,b1 = 0.9,b2 = 0.999,eps = 1e-08,eps_root = 0.0,key = 0):
+    #Key
+    key = jax.random.split(jax.random.PRNGKey(key),epochs)
+
+    #Batch size
+    bsize = int(math.floor(x.shape[0]/batches))
+
     #Optmizer NN
     optimizer = optax.adam(lr,b1,b2,eps,eps_root)
     opt_state = optimizer.init(params)
@@ -32,8 +39,17 @@ def train_morph(x,y,forward,params,loss,epochs = 1,batches = 1,lr = 0.1,b1 = 0.9
     #Train
     with alive_bar(epochs) as bar:
         for e in range(epochs):
-            opt_state,params = update(opt_state,params,x,y)
+            #Permutate x
+            x = jax.random.permutation(key[e,0])
+            for b in range(batches):
+                if b < batches - 1:
+                    xb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
+                    yb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
+                else:
+                    xb = x[b*bsize:x.shape[0],:,:]
+                    yb = y[b*bsize:y.shape[0],:,:]
+                opt_state,params = update(opt_state,params,xb,yb)
             bar.title("Loss: " + str(jnp.round(lf(params,x,y),10)))
             bar()
-
+    
     return params
