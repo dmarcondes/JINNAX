bd424906b7df68cfdcb44c5a312e86b93951e36a
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-16 11:08:21 -0600

    self-adaptative MNN

--- jinnax/training.py
+++ jinnax/training.py
@@ -37,27 +37,36 @@ def IoU_SA(true,pred,wheight):
   return wheight * (1 - (jnp.sum(2 * true * pred) + 1)/(jnp.sum(true + pred) + 1))
 
 #Training function MNN
-def train_morph(x,y,forward,params,loss,epochs = 1,batches = 1,lr = 0.001,b1 = 0.9,b2 = 0.999,eps = 1e-08,eps_root = 0.0,key = 0,notebook = False):
+def train_morph(x,y,forward,params,loss,sa = False,epochs = 1,batches = 1,lr = 0.001,b1 = 0.9,b2 = 0.999,eps = 1e-08,eps_root = 0.0,key = 0,notebook = False):
     #Key
     key = jax.random.split(jax.random.PRNGKey(key),epochs)
 
     #Batch size
     bsize = int(math.floor(x.shape[0]/batches))
 
+    #Self-adaptative
+    if sa:
+        params.append({'w': jnp.zeros((y.shape)) + 1.0})
+        @jax.jit
+        def lf(params,x,y):
+            return jnp.mean(jax.vmap(loss,in_axes = (0,0,0))(forward(x,params[:-1]),y,params[-1]['w']))
+    else:
+        #Loss function
+        @jax.jit
+        def lf(params,x,y):
+            return jnp.mean(jax.vmap(loss,in_axes = (0,0))(forward(x,params),y))
+
     #Optmizer NN
     optimizer = optax.adam(lr,b1,b2,eps,eps_root)
     opt_state = optimizer.init(params)
-
-    #Loss function
-    @jax.jit
-    def lf(params,x,y):
-        return jnp.mean(jax.vmap(loss,in_axes = (0,0))(forward(x,params),y))
 
     #Training function
     grad_loss = jax.jit(jax.grad(lf,0))
     @jax.jit
     def update(opt_state,params,x,y):
       grads = grad_loss(params,x,y)
+      if sa:
+          grads[-1]['w'] = - grads[-1]['w']
       updates, opt_state = optimizer.update(grads, opt_state)
       params = optax.apply_updates(params, updates)
       return opt_state,params
@@ -66,16 +75,19 @@ def train_morph(x,y,forward,params,loss,epochs = 1,batches = 1,lr = 0.001,b1 = 0
     t0 = time.time()
     with alive_bar(epochs) as bar:
         for e in range(epochs):
-            #Permutate x
-            x = jax.random.permutation(jax.random.PRNGKey(key[e,0]),x,0)
-            for b in range(batches):
-                if b < batches - 1:
-                    xb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
-                    yb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
-                else:
-                    xb = x[b*bsize:x.shape[0],:,:]
-                    yb = y[b*bsize:y.shape[0],:,:]
-                opt_state,params = update(opt_state,params,xb,yb)
+            if not sa:
+                #Permutate x
+                x = jax.random.permutation(jax.random.PRNGKey(key[e,0]),x,0)
+                for b in range(batches):
+                    if b < batches - 1:
+                        xb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
+                        yb = jax.lax.dynamic_slice(x,(b*bsize,0,0),(bsize,x.shape[1],x.shape[2]))
+                    else:
+                        xb = x[b*bsize:x.shape[0],:,:]
+                        yb = y[b*bsize:y.shape[0],:,:]
+                    opt_state,params = update(opt_state,params,xb,yb)
+            else:
+                opt_state,params = update(opt_state,params,x,y)
             l = str(jnp.round(lf(params,x,y),10))
             if(e % 100 == 0 and notebook):
                 print('Epoch: ' + str(e) + ' Time: ' + str(jnp.round(time.time() - t0,2)) + ' s Loss: ' + l)
