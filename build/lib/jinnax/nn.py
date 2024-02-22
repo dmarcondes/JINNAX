64399a1978326ce4295427adaa94c536850ee87b
Diego Marcondes <diegormmarcondes@gmail.com>
2024-02-22 09:23:57 -0600

    build

--- build/lib/jinnax/nn.py
+++ build/lib/jinnax/nn.py
@@ -230,7 +230,7 @@ def train_PINN(data,width,pde,test_data = None,epochs = 100,activation = jax.nn.
                 #If there is test data, compute current L2 error
                 if test_data is not None:
                     #Compute L2 error and build plots
-                    res = process_result(test_data,lambda xt: forward(xt,params),data,plot = plot,times = times,d2 = d2,save = save,file_name = file_name + '_epoch' + str(e).rjust(6, '0'),print_res = False)
+                    res = process_result(test_data,{'u': lambda xt: forward(xt,params),'time': time.time() - t0},data,plot = plot,times = times,d2 = d2,save = save,file_name = file_name + '_epoch' + str(e).rjust(6, '0'),print_res = False)
                     l = l + ' L2 error: ' + str(jnp.round(res['l2_error_test'][0],6))
                 #Print
                 print(l)
