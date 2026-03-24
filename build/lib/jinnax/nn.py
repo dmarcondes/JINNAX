a90f29a3bd0a64530c9b62633f886e9cb8a2f16b
Diego Marcondes <diegormmarcondes@gmail.com>
2026-03-25 09:12:44 +1100

    wf

--- build/lib/jinnax/nn.py
+++ build/lib/jinnax/nn.py
@@ -361,7 +361,7 @@ def eigenf_laplace(L_vec,kmax_per_axis = None,bc = "dirichlet",max_ef = None):
         if d == 1:
             max_ef = jnp.max(jnp.array(kmax_per_axis))
         else:
-            max_ef = jnp.max(2 * jnp.array(kmax_per_axis))#jnp.max(d * jnp.array(kmax_per_axis))
+            max_ef = jnp.max(d * jnp.array(kmax_per_axis))
 
     #Build the candidate multi-indices per axis
     kmax_per_axis = list(map(int, kmax_per_axis))
